da19965e17dd372b3a62c4ca120f409b4c981d81
Amith B V <amithrao0606@gmail.com>
2026-08-12 09:11:34 +0530

    working with numbers

---
+++ 10-Numbers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Binary Numbers :
+    int number1 = 0b11111111;
+    cout << number1 << endl;
+
+    // Hexa-decimal Numbers :
+    int number2 = 0xff;
+    cout << number2 << endl;
+
+    return 0;
+}
